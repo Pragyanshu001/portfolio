12056b3dcab24bee893e9f010891777f1bf3a27b
Pragyanshu Gupta <117273832+Pragyanshu001@users.noreply.github.com>
2025-11-12 11:19:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ gsap
     scrollTrigger: {
       trigger: "#parallax",
       start: "top top",
-      end: "+=1500", // scroll distance for animation
+      end: "+=3500", // scroll distance for animation
       //   end: "+=100%",
       scrub: true,
       pin: true, // page scroll will pause until animation completes
